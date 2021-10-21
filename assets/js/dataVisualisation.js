c7399bd886bb31772a43daba252ab4610ec28351
Mess Dorchies <messaouddorchies@gmail.com>
2021-10-21 11:50:01 +0200

    commitThursday

--- assets/js/dataVisualisation.js
+++ assets/js/dataVisualisation.js
@@ -1,10 +1,21 @@
-let dataPoint = [];
-fetch('https://canvasjs.com/services/data/datapoints.php').then(function(response) {
-    response.json().then(function(json) {
-      dataPoint.push(json);
-    });
-  });
-  console.log(dataPoint);
+// let dataPoint = [];
+// fetch('https://canvasjs.com/services/data/datapoints.php').then(function(response) {
+//     response.json().then(function(json) {
+//       dataPoint.push(json);
+//     });
+//   });
+//   console.log(dataPoint);
 
-  let table1 = document.getElementById('table1');
-  let table2 = document.getElementById('table2');
+const svg = d3.select('svg');
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+d3.csv('https://canvasjs.com/services/data/datapoints.php').then(data => {
+  data.forEach(data => {
+    dataPoint.push(data);
+  });
+})
+  
+let table1 = document.getElementById('table1');
+  
+let table2 = document.getElementById('table2');
